b3da257a50b336c944b03a4c44ca9e86a92bbdd7
arbajkhan97855 <pathanarbaj03328@gmail.com>
2026-08-06 15:28:34 +0530

    Added payment test route

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -15,6 +15,13 @@ router.post(
     createPaymentOrder
 );
 
+router.get("/test", (req, res) => {
+    res.json({
+        success: true,
+        message: "Payment Route Working"
+    });
+});
+
 router.post(
     "/verify",
     authMiddleware,
